6caef63e46b059ede2135c0341f8864675bc6a11
Fuuks <fukiwony@gmail.com>
2024-09-17 20:56:02 +0200

    test

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = "example-instance"
-  }
-}
