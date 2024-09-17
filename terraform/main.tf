11d3778673c0867dd115fbf000e0331ecf75b4b7
Fuuks <fukiwony@gmail.com>
2024-09-17 22:00:28 +0200

    fix feature #5

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Création du bucket S3
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "lecloudfacile-myname-05"  # Remplacez par un nom unique
+  bucket = "lecloudfacile-myname-06"  # Remplacez par un nom unique
 }
 
 # Politique du bucket pour permettre l'accès public en lecture
